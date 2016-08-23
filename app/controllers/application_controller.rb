489735ab8e549e8734d78a84138c580dc06a8b5b
romgripper <xb1101101@gmail.com>
2016-08-23 14:05:07 +0800

    add parent name | Logout to the up right corner if logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :ensure_login
+  helper_method :logged_in?, :current_parent
 
   protected
   	def ensure_login
   		redirect_to login_path unless session[:parent_id]
+  	end
+
+  	def logged_in?
+  		session[:parent_id]
+  	end
+
+  	def current_parent
+  		@current_parent ||= Parent.find(session[:parent_id])
   	end
 end
