f71a7c84dc8de362366a03c25bb90dc549239ea6
romgripper <xb1101101@gmail.com>
2016-08-24 15:07:55 +0800

    make sure parent_profile is destroyed when a parent is removed

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,6 +1,6 @@
 class Parent < ApplicationRecord
 	has_secure_password
-	has_one :parent_profile
+	has_one :parent_profile, dependent: :destroy
 	has_and_belongs_to_many :babies
 
 	validates :username, :password, presence: true
